bba1a4519b653cdd85a00ce78b7dacbdc8b4e126
Tobias Kolb <tobias-kolb@hotmail.de>
2019-07-22 19:16:51 +0000

    001/google calendar step 2

--- content/en/page/calendar.md
+++ content/en/page/calendar.md
@@ -14,5 +14,4 @@ title: Calendar
 <ul id="events-past">
 </ul>
 
-<iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FBerlin&amp;src=eGhhaW4uY2FsQGdtYWlsLmNvbQ&amp;color=%23039BE5&amp;mode=MONTH&amp;showTitle=0&amp;showNav=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;showTz=0" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
+<div id='calendar'></div>
--- themes/xhain-theme/static/js/custom.js
+++ themes/xhain-theme/static/js/custom.js
@@ -1,1 +1,31 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var initialLocaleCode = 'en';
+    var localeSelectorEl = document.getElementById('locale-selector');
+    var calendarEl = document.getElementById('calendar');
 
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'googleCalendar' ],
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridMonth,timeGridDay,listMonth',
+        locale: 'en'
+      },
+      locale: initialLocaleCode,
+      buttonIcons: true, // show the prev/next text
+      weekNumbers: true,
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      googleCalendarApiKey: 'AIzaSyAhG_Ds6PvH1YQZ2ZSKg2GhUgO-CsLdOWw',
+      events: {
+        googleCalendarId: 'xhain.cal@gmail.com'
+      }
+    });
+
+    calendar.render();
+
+    if(window.location.href.indexOf('/de/') > 0){
+        calendar.setOption('locale', 'de');
+    }
+  });
