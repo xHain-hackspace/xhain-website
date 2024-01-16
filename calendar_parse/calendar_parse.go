7c52dba29092613cc0bb2b66fe6a0d4ef55b8868
Felix Delattre <felix@delattre.de>
2024-01-16 22:34:12 +0000

    Switched to calendar-bot for parsing calendar events.

--- calendar_parse/calendar_parse.go
+++ calendar_parse/calendar_parse.go
@@ -3,15 +3,12 @@ package main
 import (
 	"bytes"
 	"html/template"
-	"io"
 	"log"
-	"net/http"
 	"os"
-	"sort"
 	"strings"
 	"time"
 
-	"github.com/apognu/gocal"
+	"github.com/gueldenstone/calendar-bot/pkg/calendar"
 )
 
 func main() {
@@ -23,18 +20,12 @@ func main() {
 	timerange_end := timerange_start.AddDate(0, 2, -1)
 
 	// Fetch and read ICS file
-	data, err := fetchICalData(url)
-	if err != nil {
-		log.Printf("Failed to fetch or read the ICS file: %v\n", err)
-		return
-	}
-
-	// Parse ICS file
-	events, err := parseICSData(data, timerange_start, timerange_end)
+	events, err := getICSData(url, timerange_start, timerange_end)
 	if err != nil {
 		log.Printf("Failed to parse the ICS file: %v\n", err)
 		return
 	}
+
 	// Render HTML
 	generatedHTML, err := generateHTML(events, timerange_start, timerange_end)
 	if err != nil {
@@ -52,8 +43,16 @@ func main() {
 	}
 }
 
+type EventData struct {
+	Start       time.Time
+	End         time.Time
+	Summary     string
+	Location    string
+	Description string
+}
+
 type DayData struct {
-	Events  []gocal.Event
+	Events  []EventData
 	Weekday time.Weekday
 	Date    time.Time
 }
@@ -64,38 +63,116 @@ type TemplateData struct {
 	HtmlWrapper []template.HTML
 }
 
-func fetchICalData(url string) ([]byte, error) {
-	resp, err := http.Get(url)
+func getICSData(url string, start time.Time, end time.Time) (map[int]map[string]map[int]DayData, error) {
+
+	// Download and read ICS file
+	calendar, err := calendar.NewCalendar(url, time.Local, log.Default())
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
 
-	icsData, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
+	events := make(map[int]map[string]map[int]DayData)
+
+	// Loop through days from start to end
+	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
+		year := d.Year()
+		month := d.Format("01")
+		day := d.Day()
+
+		// Initialize maps if necessary
+		if events[year] == nil {
+			events[year] = make(map[string]map[int]DayData)
+		}
+		if events[year][month] == nil {
+			events[year][month] = make(map[int]DayData)
+		}
+
+		// If the day does not exist in the map, create a DayData with no events
+		if _, exists := events[year][month][day]; !exists {
+			events[year][month][day] = DayData{
+				Events:  []EventData{},
+				Weekday: d.Weekday(),
+				Date:    d,
+			}
+		}
+
+		// Fetch events for the day
+		dailyICSEvents, err := calendar.GetEventsOn(d)
+		if err != nil {
+			return nil, err
+		}
+
+		// Convert ical.Event to EventData
+		for _, icalEvent := range dailyICSEvents {
+
+			eventStart, err := icalEvent.Props.DateTime("DTSTART", time.Local)
+			if err != nil {
+				return nil, err
+			}
+			eventData := EventData{
+				Start: eventStart,
+			}
+
+			// Fetch end time, summary, location, description if they exist
+			if endProp, ok := icalEvent.Props["DTEND"]; ok && len(endProp) > 0 {
+				eventEnd, err := endProp[0].DateTime(time.Local)
+				if err != nil {
+					return nil, err
+				}
+				eventData.End = eventEnd
+			}
+
+			if summaryProp, ok := icalEvent.Props["SUMMARY"]; ok && len(summaryProp) > 0 {
+				eventData.Summary, err = summaryProp[0].Text()
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			if locationProp, ok := icalEvent.Props["LOCATION"]; ok && len(locationProp) > 0 {
+				eventData.Location, err = locationProp[0].Text()
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			if descriptionProp, ok := icalEvent.Props["DESCRIPTION"]; ok && len(descriptionProp) > 0 {
+				eventData.Description, err = descriptionProp[0].Text()
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			// Append eventData to the day
+			yearKey := eventData.Start.Year()
+			monthKey := eventData.Start.Format("01")
+			dayKey := eventData.Start.Day()
+
+			if _, ok := events[yearKey]; !ok {
+				events[yearKey] = make(map[string]map[int]DayData)
+			}
+			if _, ok := events[yearKey][monthKey]; !ok {
+				events[yearKey][monthKey] = make(map[int]DayData)
+			}
+			if _, ok := events[yearKey][monthKey][dayKey]; !ok {
+				events[yearKey][monthKey][dayKey] = DayData{
+					Date:    eventData.Start,
+					Events:  []EventData{},
+					Weekday: eventData.Start.Weekday(),
+				}
+			}
+
+			dayData := events[yearKey][monthKey][dayKey]
+			dayData.Events = append(dayData.Events, eventData)
+			events[yearKey][monthKey][dayKey] = dayData
+		}
+
 	}
-	return icsData, nil
+
+	return events, nil
 }
 
-func parseICSData(icsData []byte, start time.Time, end time.Time) ([]gocal.Event, error) {
-	c := gocal.NewParser(bytes.NewReader(icsData))
-
-	c.Start, c.End = &start, &end
-
-	if err := c.Parse(); err != nil {
-		return nil, err
-	}
-
-	// Sort the events by their start date
-	sort.Slice(c.Events, func(i, j int) bool {
-		return c.Events[i].Start.Before(*c.Events[j].Start)
-	})
-
-	return c.Events, nil
-}
-
-func generateHTML(events []gocal.Event, start time.Time, end time.Time) (string, error) {
+func generateHTML(events map[int]map[string]map[int]DayData, start time.Time, end time.Time) (string, error) {
 
 	// Define template functions
 	funcMap := template.FuncMap{
@@ -120,13 +197,9 @@ func generateHTML(events []gocal.Event, start time.Time, end time.Time) (string,
 		return "", err
 	}
 
-	// Main data preparation
-	preparedEvents := groupEvents(events)
-	preparedEvents = addEmptyDaysToEvents(preparedEvents, start, end)
-
 	templateData := TemplateData{
 		Now:         time.Now(),
-		Events:      preparedEvents,
+		Events:      events,
 		HtmlWrapper: []template.HTML{"{{< rawhtml >}}", "{{< /rawhtml >}}"},
 	}
 
@@ -140,61 +213,6 @@ func generateHTML(events []gocal.Event, start time.Time, end time.Time) (string,
 	htmlString = strings.ReplaceAll(htmlString, "\\n", "<br>")
 
 	return htmlString, nil
-}
-
-func groupEvents(events []gocal.Event) map[int]map[string]map[int]DayData {
-
-	// Group events by year, month, and day
-	groupedEvents := make(map[int]map[string]map[int]DayData)
-
-	for _, e := range events {
-		year := e.Start.Year()
-		month := e.Start.Format("01")
-		day := e.Start.Day()
-
-		if groupedEvents[year] == nil {
-			groupedEvents[year] = make(map[string]map[int]DayData)
-		}
-
-		if groupedEvents[year][month] == nil {
-			groupedEvents[year][month] = make(map[int]DayData)
-		}
-
-		dayData := groupedEvents[year][month][day]
-		dayData.Events = append(dayData.Events, e)
-		dayData.Weekday = e.Start.Weekday()
-		if dayData.Date.IsZero() {
-			dayData.Date = *e.Start
-		}
-		groupedEvents[year][month][day] = dayData
-	}
-
-	return groupedEvents
-}
-
-func addEmptyDaysToEvents(events map[int]map[string]map[int]DayData, start, end time.Time) map[int]map[string]map[int]DayData {
-	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
-		year := d.Year()
-		month := d.Format("01")
-		day := d.Day()
-
-		if events[year] == nil {
-			events[year] = make(map[string]map[int]DayData)
-		}
-
-		if events[year][month] == nil {
-			events[year][month] = make(map[int]DayData)
-		}
-
-		if _, exists := events[year][month][day]; !exists {
-			events[year][month][day] = DayData{
-				Events:  []gocal.Event{},
-				Weekday: d.Weekday(),
-				Date:    d,
-			}
-		}
-	}
-	return events
 }
 
 func writeOutput(generatedHTML, filePath string) error {
--- calendar_parse/go.mod
+++ calendar_parse/go.mod
@@ -5,4 +5,7 @@ go 1.21.5
 require (
 	github.com/ChannelMeter/iso8601duration v0.0.0-20150204201828-8da3af7a2a61 // indirect
 	github.com/apognu/gocal v0.9.0 // indirect
+	github.com/emersion/go-ical v0.0.0-20220601085725-0864dccc089f // indirect
+	github.com/gueldenstone/calendar-bot v1.0.7 // indirect
+	github.com/teambition/rrule-go v1.7.2 // indirect
 )
